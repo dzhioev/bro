b1e2f875a569720fe83c74607d962c342393b8c3
Pavel Sergeev <dzhioev@gmail.com>
2025-07-14 20:06:59 +0300

    simplify yesno and make it react to Ctrl + C
    > rewritten from a private repository (original: 8b0c0ee428c5)

--- bro/bro/base/yesno.py
+++ bro/bro/base/yesno.py
@@ -1,43 +1,29 @@
-import sys
-import tty
-import termios
-
 YES = True
 NO = False
-
-def getch():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(fd)
-        ch = sys.stdin.read(1).lower()
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
 
 def yesno(question: str, default=NO) -> bool:
     if default == YES:
         prompt = f"{question} [Y/n]: "
+        default_char = 'Y'
     else:
         prompt = f"{question} [y/N]: "
+        default_char = 'N'
     
     while True:
-        sys.stdout.write(prompt)
-        sys.stdout.flush()
-        
-        key = getch()
-        
-        if key in ['\r', '\n']:
-            if default == YES:
-                sys.stdout.write('y\n')
+        try:
+            response = input(prompt).strip().lower()
+            
+            if response == '':
+                return default
+            elif response in ['y', 'yes']:
+                return True
+            elif response in ['n', 'no']:
+                return False
             else:
-                sys.stdout.write('n\n')
+                print("Please enter 'y(es)' or 'n(o)' (or press Enter for default)")
+        except KeyboardInterrupt:
+            print("\nInterrupted!")
+            raise
+        except EOFError:
+            print(f"\n{default_char}")
             return default
-        elif key == 'y':
-            sys.stdout.write('y\n')
-            return True
-        elif key == 'n':
-            sys.stdout.write('n\n')
-            return False
-        
-        sys.stdout.write('\n')
