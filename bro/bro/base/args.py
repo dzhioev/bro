45e88aca49a37dffe4085a51b806cd3ef8fa02b6
Pavel Sergeev <dzhioev@gmail.com>
2025-11-01 03:10:11 +0000

    introduce args.Parser
    > rewritten from a private repository (original: dfcea4e06679)

--- bro/bro/base/args.py
+++ bro/bro/base/args.py
@@ -1,11 +1,46 @@
 #!/usr/bin/env python
 
 import argparse
-from base.time_util import _parse_date
+from base.time_util import parse_moment, datetime
+from icecream import ic
+from typing import Callable, Type
+import logging
 
-def date_parser(arg):
+
+def moment_parser(arg: str) -> datetime:
     try:
-        return _parse_date(arg)
+        return parse_moment(arg)
     except ValueError:
-        raise argparse.ArgumentTypeError(f"Invalid date format: {arg}. Use YYYY-MM-DD.")
+        raise argparse.ArgumentTypeError(f"invalid date format: {arg}")
+
+
+def trigger(fn: Callable) -> Type[argparse.Action]:
+
+  class TriggerAction(argparse.Action):
+    def __init__(self, option_strings, dest, **kwargs):
+      kwargs.setdefault('nargs', 0)
+      super().__init__(option_strings, dest, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+      fn()
+
+  return TriggerAction
+
+
+def enable_verbose_logging() -> None:
+  logging.basicConfig(level=logging.DEBUG)
+
+
+class Parser(argparse.ArgumentParser):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+
+    self.add_argument('--verbose',
+                      action=trigger(enable_verbose_logging),
+                      help='enable verbose logging')
+
+  def parse_args(self, args=None, namespace=None):
+    ns = super().parse_args(args, namespace)
+    delattr(ns, 'verbose')
+    return ns
 
--- bro/bro/base/time_util.py
+++ bro/bro/base/time_util.py
@@ -1,10 +1,68 @@
 #!/usr/bin/env python
-import datetime
 import pytz
+from datetime import datetime
+import sys
+import base.args
 
-def _parse_date(date: str) -> datetime.datetime:
-  return pytz.utc.localize(datetime.datetime.strptime(date, '%Y-%m-%d'))
+UTC = pytz.utc
+timezone = pytz.timezone
 
-PAST = _parse_date('1988-01-01')
-FUTURE = _parse_date('2188-01-01')
+def parse_day(s: str) -> datetime:
+  return UTC.localize(datetime.strptime(s, '%Y-%m-%d'))
+
+def parse_time(s: str) -> datetime:
+  return UTC.localize(datetime.strptime(s, '%Y-%m-%d %H:%M:%S'))
+
+def format_time(d: datetime, show_tz_info: bool = False) -> str:
+  template = '%Y-%m-%d %H:%M:%S'
+  if show_tz_info:
+    tzname = d.tzname()
+    template += f' {tzname}%z'
+  return d.strftime(template)
+
+def utc_now() -> datetime:
+  return datetime.now(tz=UTC)
+
+def parse_moment(s: str) -> datetime:
+  if s == 'now':
+    return utc_now()
+  try:
+    return parse_time(s)
+  except:
+    pass
+  try:
+    return parse_day(s)
+  except:
+    pass
+  raise ValueError('invalid moment format')
+
+
+PAST = parse_day('1988-01-01')
+FUTURE = parse_day('2188-01-01')
+
+
+def format_now(show_tz_info: bool, zone: str | None) -> str:
+  now = utc_now()
+
+  if zone is not None:
+    now = now.astimezone(timezone(zone))
+
+  return format_time(now, show_tz_info=show_tz_info)
+
+def print_current_time(show_tz_info: bool, zone: str | None) -> None:
+  current_time = format_now(show_tz_info, zone)
+  print(current_time)
+
+
+def main(argv):
+  parser = base.args.Parser(description='print current time')
+  parser.add_argument('-z', dest='show_tz_info',
+                      help='show tz info', action='store_true')
+  parser.add_argument('--zone', help='convert time to the timezone')
+  args = parser.parse_args(argv[1:])
+  return print_current_time(**vars(args))
+
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv))
 
