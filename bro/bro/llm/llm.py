bffb1a9e6bb44cc8218e42cf260d53d3596fe96b
Pavel Sergeev <dzhioev@gmail.com>
2025-11-02 17:08:34 +0000

    refactored bro
    > rewritten from a private repository (original: aba5131d515d)

--- bro/bro/llm/llm.py
+++ bro/bro/llm/llm.py
@@ -1,32 +1,55 @@
+#!/usr/bin/env python
+
+import base.args
+
 import abc
-from typing import Any, Optional
+import sys
+import asyncio
 
 class Bro(abc.ABC):
     @abc.abstractmethod
-    async def tell(self, data: Any) -> None:
-        '''Send data to the model.'''
+    async def tell(self, phrase: str) -> None:
         pass
 
     @abc.abstractmethod
-    async def ask(self) -> Any:
-        '''Get the model output.'''
+    async def ask(self) -> str:
         pass
-
 
 class EchoBro(Bro):
     def __init__(self):
-        self.data = None
+        self.phrase = None
 
-    async def tell(self, data: Any) -> None:
-        self.data = data
+    async def tell(self, phrase: str) -> None:
+        self.pharse = phrase
 
-    async def ask(self) -> Any:
-        return f'Hello, world!: "{self.data}"!'
+    async def ask(self) -> str:
+        return f'Hello, world!: "{self.phrase}"!'
 
-def get_bro(type: Optional[str], *args, **kwargs) -> Bro:
-    if type is None:
-        return EchoBro()
+
+def get_bro(type: str, *args, **kwargs) -> Bro:
+    if type == 'echo':
+      return EchoBro(*args, **kwargs)
     if type == 'chat_gpt':
-        import bros.chat_gpt_bro
-        return bros.chat_gpt_bro.ChatGPTBro(*args, **kwargs)
+      import bro.bros
+      return bro.bros.ChatGPTBro.create(*args, **kwargs)
     raise ValueError(f'Unknown bro type: {type}')
+
+async def bro_main(bro_type: str, *args, **kwargs):
+  bro = get_bro(bro_type, *args, **kwargs)
+  for _ in range(5):
+    to_tell = f'ping {_}'
+    print(f'> {to_tell}')
+    await bro.tell(to_tell)
+    print()
+    asked = await bro.ask()
+    print(f'< {asked}')
+    print()
+
+def main(argv) -> int | None:
+  parser = base.args.Parser(description='Chat with bro')
+  parser.add_argument('bro_type', nargs='?', default='echo')
+  kwargs = parser.parse(argv)
+  return asyncio.run(bro_main(**kwargs))
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv))
--- bro/bro/llm/llms/chat_gpt.py
+++ bro/bro/llm/llms/chat_gpt.py
@@ -1,15 +1,25 @@
-from bro import Bro
+import bro.bro
+import configs
+import json
+import os
 from openai import OpenAI
 
+DEFAULT_CONFIG_PATH = os.path.join(configs.DEFAULT_CONFIGS_DIR, 'openai.json')
 
-class ChatGPTBro(Bro):
+class ChatGPTBro(bro.bro.Bro):
+    @staticmethod
+    def create(config_path=DEFAULT_CONFIG_PATH):
+      with open(config_path, 'r') as f:
+        config = json.load(f)
+      return ChatGPTBro(api_key=config['api_key'])
+
     def __init__(self, api_key: str):
-        self.client = OpenAI(api_key=api_key)
+      self.client = OpenAI(api_key=api_key)
 
-    async def tell(self, prompt: str) -> None:
+    async def tell(self, phrase: str) -> None:
         self.response = self.client.chat.completions.create(
             model="gpt-4-turbo",
-            messages=[{"role": "user", "content": prompt},],
+            messages=[{"role": "user", "content": phrase},],
             max_tokens=500)
 
     async def ask(self) -> str:
